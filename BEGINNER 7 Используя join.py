dcdb33ac658c22063d4eb8c3c9e897d8fc3b51e5
7SoKKoS7 <97355065+7SoKKoS7@users.noreply.github.com>
2022-01-10 01:41:35 +0200

    Create BEGINNER 7 Используя join.py

---
+++ BEGINNER 7 Используя join.py
@@ -0,0 +1,5 @@
+# Дан список из строк. Используя join, соедините строки так, чтобы они были разделены через запятую.
+# На выходе должна получиться строка в виде "my_string1,my_string2,my_string3"
+list_str = ['my_string1','my_string2','my_string3','my_string4','my_string5','my_string6','my_string7','my_string8','my_string9','my_string10','my_string11']
+string_str = ','.join(list_str)
+print('String from list - ', string_str)
